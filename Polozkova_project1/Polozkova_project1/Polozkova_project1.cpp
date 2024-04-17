03305a49e9143ae4086a33de6190ee93828d99c2
4nespes2 <katena-polozkova@mail.ru>
2024-04-17 10:46:18 +0300

    new files

---
+++ Polozkova_project1/Polozkova_project1/Polozkova_project1.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int inputx() {
+
+}
+
+int inputa() {
+
+}
+
+bool XnotinA(int x, int a) {
+
+}
+
+bool XdoubleA(int x, int a) {
+
+}
+
+int main() {
+    int choice;
+    int X, A;
+
+    do {
+        cout << "Выберите действие:\n";
+        cout << "1. Ввод натурального числа X\n";
+        cout << "2. Ввод цифры A\n";
+        cout << "3. Проверка отсутствия цифры A в числе X\n";
+        cout << "4. Проверка наличия цифры A более чем 2 раза в числе X\n";
+        cout << "5. Выход\n";
+        cin >> choice;
+
+        switch (choice) {
+        case 1:
+            X = inputx();
+            break;
+        case 2:
+            A = inputa();
+            break;
+        case 3:
+            if (XnotinA(X, A)) {
+                cout << "Цифра " << A << " отсутствует в числе " << X << endl;
+            }
+            else {
+                cout << "Цифра " << A << " присутствует в числе " << X << endl;
+            }
+            break;
+        case 4:
+            if (XdoubleA(X, A)) {
+                cout << "Цифра " << A << " встречается более 2 раз в числе " << X << endl;
+            }
+            else {
+                cout << "Цифра " << A << " встречается максимум 2 раза в числе " << X << endl;
+            }
+            break;
+        case 5:
+            cout << "Выход из программы.\n";
+            break;
+        default:
+            cout << "Некорректный выбор. Попробуйте снова.\n";
+        }
+    } 
+    
+    while (choice != 5);
+
+    return 0;
+}
+
+
